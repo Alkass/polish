01b0509cbeb27c3a3eabf71d7910bf0d710215af
alkass <fadi.eliya@yahoo.com>
2017-05-09 21:48:32 -0500

    .

--- scripts/codecov-reporting.sh
+++ scripts/codecov-reporting.sh
@@ -8,7 +8,7 @@ make
 sudo make install
 cd ../..
 rm -rf kcov-master
-for file in target/debug/polish-*[^\.d]; do
+for file in target/debug/polish-*.d; do
   mkdir -p "target/cov/$(basename $file)"
   kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"
 done
