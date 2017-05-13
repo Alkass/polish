245e783d9b75d8ee0b986326c2e650531324a898
Fadi Hanna Al-Kass <AlKass@users.noreply.github.com>
2017-05-12 21:19:15 -0500

    Create install.ps1

---
+++ scripts/install.ps1
@@ -0,0 +1,13 @@
+if ($env:PLATFORM -eq "x86") {
+    $arch = "i686"
+}
+else {
+    $arch = "x86_64"
+}
+
+$rust_version = $env:RUST_VERSION
+$rust_install = "rust-$rust_version-$arch-pc-windows-gnu.msi"
+Start-FileDownload "https://static.rust-lang.org/dist/$rust_install" -FileName $rust_install
+Start-Process -FilePath msiexec -ArgumentList /i, $rust_install, /quiet, INSTALLDIR="C:\Rust" -Wait
+$env:Path = $env:Path + ";C:\Rust\bin"
+rustc -vV
