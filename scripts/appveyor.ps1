edcec307d233bb43fcce9644046d0c852d366ed9
alkass <fadi.eliya@yahoo.com>
2017-05-14 11:50:04 -0500

    removed excess

--- scripts/appveyor.ps1
+++ scripts/appveyor.ps1
@@ -11,8 +11,3 @@ Start-FileDownload "https://static.rust-lang.org/dist/$rust_install" -FileName $
 Start-Process -FilePath msiexec -ArgumentList /i, $rust_install, /quiet, INSTALLDIR="C:\Rust" -Wait
 $env:Path = $env:Path + ";C:\Rust\bin"
 rustc -vV
-
-set PATH=C:\msys64\mingw%BITS%\bin;C:\msys64\usr\bin;%PATH%
-if defined MINGW_URL appveyor DownloadFile %MINGW_URL%/%MINGW_ARCHIVE%
-if defined MINGW_URL 7z x -y %MINGW_ARCHIVE% > nul
-if defined MINGW_URL set PATH=%CD%\%MINGW_DIR%\bin;C:\msys64\usr\bin;%PATH%
