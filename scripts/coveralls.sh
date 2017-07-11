cb23302a7310f84f3b428c26ead81531b1d47874
Fadi Hanna Al-Kass <AlKass@users.noreply.github.com>
2017-07-11 13:35:30 -0500

    Delete coveralls.sh

--- scripts/coveralls.sh
+++
@@ -1,7 +0,0 @@
-wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
-tar xzf master.tar.gz && mkdir kcov-master/build && cd kcov-master/build
-cmake ..
-make
-sudo make install
-cd ../..
-kcov --coveralls-id=$1 --exclude-pattern=/.cargo target/kcov target/debug/polish*
